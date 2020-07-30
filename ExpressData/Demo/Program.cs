bc6f2e342f14fcfefb24cd22f61e3fa71d4c03cf
Sangeeth Nandakumar <sangeethnandakumarwork@gmail.com>
2020-07-31 02:40:05 +0530

    Demo fixes

--- ExpressData/Demo/Program.cs
+++ ExpressData/Demo/Program.cs
@@ -15,44 +15,48 @@ namespace Demo
             Console.WriteLine("2. Create 3 columns Id, Fname and Lname");
             Console.WriteLine("3. Set the connection string to database server inside Main()");
             Console.WriteLine("------------------------------------------------------------------------------");
-            Console.ReadLine();
+
 
             //Prerequesties
-            var _connectionString = "";
-
-
-            #region QUERIES
-            //Plain Query - Gives first result
-            var sql1 = $"SELECT * FROM tblUsers WHERE Id=1";
-            var result = SqlHelper.Query<User>(sql1, _connectionString).FirstOrDefault();
-
-            //Safe Query - Parameterised query
-            var sql2 = $"SELECT * FROM tblUsers WHERE Id=@id AND Fname=@fname";
-            result = SqlHelper.QuerySafe<User>(sql2, new
-            {
-                id = 1,
-                fname = "Sangeeth"
-            }, _connectionString);
-            #endregion
+            var _connectionString = @"Server=DESKTOP-708EN4A\SQLEXPRESS;Database=TIS;Trusted_Connection=True;";
 
             #region INSERT AND UPDATES
-            var record = new User
+            //Insert
+            var record = new tblUser
             {
                 Id = 1,
                 FName = "Sangeeth",
                 LName = "Nandakumar"
-            };
-
-            //Insert
-            var primaryKey = SqlHelper.Insert<User>(record, _connectionString);
+            };            
+            var primaryKey = SqlHelper.Insert<tblUser>(record, _connectionString);
 
             //Update
-            var isUpdated = SqlHelper.Insert<User>(record, _connectionString);
+            record = new tblUser
+            {
+                Id = 1,
+                FName = "Navaneeth",
+                LName = "Nandakumar"
+            };            
+            var isUpdated = SqlHelper.Update<tblUser>(record, _connectionString);
             #endregion
+
+            #region QUERIES
+            //Plain Query - Gives first result
+            var sql1 = $"SELECT * FROM tblUser WHERE Id=1";
+            var result = SqlHelper.Query<tblUser>(sql1, _connectionString).FirstOrDefault();
+
+            //Safe Query - Parameterised query
+            var sql2 = $"SELECT * FROM tblUser WHERE Id=@id AND Fname=@fname";
+            result = SqlHelper.QuerySafe<tblUser>(sql2, new
+            {
+                id = 1,
+                fname = "Navaneeth"
+            }, _connectionString);
+            #endregion            
 
             #region STORED PROCEDURES
             var procedureName = $"spGetUsers";
-            result = SqlHelper.Execute<User>(procedureName, new { id = 1 }, _connectionString).FirstOrDefault();
+            result = SqlHelper.Execute<tblUser>(procedureName, new { id = 1 }, _connectionString).FirstOrDefault();
             #endregion
         }
     }
--- ExpressData/Demo/tblUser.cs
+++ ExpressData/Demo/tblUser.cs
@@ -6,7 +6,7 @@ using System.Text;
 namespace Demo
 {
     [Table("tblUser")]
-    public class User
+    public class tblUser
     {
         [Key]
         public int Id { get; set; }
